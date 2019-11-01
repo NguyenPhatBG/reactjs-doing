cc780cbe3a22e8057c63139a6114369b8f75ebfe
joon_young_92 <suhjoon92@gmail.com>
2019-11-01 12:33:03 +0700

    Changing State (and this)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    hello: 'Hello Friend!'
+  };
+  handleClick = (e) => {
+    console.log(this.state);
+    this.setState({ hello: 'Hello Phat!' });
+  }
+  handleMouseOver = (e) => {
+    console.log(e.target, e.pageX);
+  }
+  hanleCopy = (e) => {
+    console.log('Trying to copy this.');
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.hello}</h1>
+        <button onClick={this.handleClick}>Click M1</button>
+        <button onMouseOver={this.handleMouseOver}>Mouse Over</button>
+        <p onCopy={this.hanleCopy}>Copy text from me.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
