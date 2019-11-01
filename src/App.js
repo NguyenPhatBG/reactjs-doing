9472efe6c4d06bc8674120981c0ec592ff4eda01
joon_young_92 <suhjoon92@gmail.com>
2019-11-01 13:37:56 +0700

    Intro to Forms

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import "./App.css";
 
 class App extends React.Component {
   state = {
-    hello: 'Hello Friend!'
+    name: 'Phat'
   };
-  handleClick = (e) => {
-    console.log(this.state);
-    this.setState({ hello: 'Hello Phat!' });
+
+  onHandleChange = (e) => {
+    this.setState({
+      name: e.target.value
+    });
   }
-  handleMouseOver = (e) => {
-    console.log(e.target, e.pageX);
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted: ', this.state.name);
   }
-  hanleCopy = (e) => {
-    console.log('Trying to copy this.');
-  }
+
   render() {
     return (
       <div className="App">
-        <h1>{this.state.hello}</h1>
-        <button onClick={this.handleClick}>Click M1</button>
-        <button onMouseOver={this.handleMouseOver}>Mouse Over</button>
-        <p onCopy={this.hanleCopy}>Copy text from me.</p>
+      <h4>My name is {this.state.name}</h4>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.onHandleChange} />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
